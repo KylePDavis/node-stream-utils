2a40321b04075bde5b28f59304935fbc62dee8cc
Kyle P. Davis <me@KylePDavis.com>
2012-10-24 15:39:54 -0400

    Updated pipeline tests to check no args scenario

--- test/lib/pipeline.js
+++ test/lib/pipeline.js
@@ -7,6 +7,25 @@ exports = module.exports = {
 
 	"pipeline": {
 
+		"should be able to combine 0 no-op Streams without side effects (and it should be reusable)": function(){
+			var i = [1, 2, 3, 4, 5],
+				e = [1, 2, 3, 4, 5],
+				s = pipeline(),
+				a = [];
+			s.on("data", a.push.bind(a));
+			i.forEach(s.write.bind(s));
+			s.end();
+			assert.equal(JSON.stringify(a), JSON.stringify(e), "Unexpected value!");
+			assert.deepEqual(a, e, "Unexpected value (not deepEqual)!");
+			// Ensure that it's reusable after reset, too
+			s.reset();
+			a.length = 0;	// clear the array
+			i.forEach(s.write.bind(s));
+			s.end();
+			assert.equal(JSON.stringify(a), JSON.stringify(e), "Unexpected value!");
+			assert.deepEqual(a, e, "Unexpected value (not deepEqual)!");
+		},
+
 		"should be able to combine 1 no-op Stream without side effects (and it should be reusable)": function(){
 			var i = [1, 2, 3, 4, 5],
 				e = [1, 2, 3, 4, 5],
